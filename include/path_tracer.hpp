475ba65065de54046bdcfc5b8f9c1062533230fa
rcy17 <rcycyzh@163.com>
2020-06-15 15:29:03 +0800

    Use Mento Carlo

--- include/path_tracer.hpp
+++ include/path_tracer.hpp
@@ -21,53 +21,61 @@ public:
         Hit hit(FLT_MAX, NULL, Vector3f(0, 0, 0));
         const auto group = scene->getGroup();
 
-        if (bounces < max_bounces && group->intersect(ray, hit, t_min))
-        {
-            auto p = ray.pointAtParameter(hit.getT());
-            auto material = hit.getMaterial();
-            auto normal = hit.getNormal().normalized();
-            auto incoming = ray.getNormalizedDirection();
-            Vector3f color = material->getColor(hit, p);
-            Vector3f direction;
-            double eta = material->getRefractionIndex();
-            switch (material->getType())
-            {
-            case ILLUMINANT:
-                // do nothing
-                break;
-            case DIFFUSE:
-                direction = hit.getRandomReflect(incoming, seed);
-                color = color * traceRay({p, direction}, EPSILON, bounces + 1, seed);
-                break;
-            case SPECULAR:
-                direction = mirrorDirection(normal, incoming);
-                color = color * traceRay({p, direction}, EPSILON, bounces + 1, seed);
-                break;
-            case REFRACTIVE:
-            {
-                const double eps = 1e-2;
-                double eta = material->getRefractionIndex();
-                assert(eta > 1);
-                if (Vector3f::dot(normal, incoming) < 0)
-                    eta = 1 / eta;
-                auto reflect_direction = mirrorDirection(normal, incoming);
-                if (!transmittedDirection(normal, incoming, eta, 1, direction))
-                    color = color * traceRay({p, reflect_direction}, EPSILON, bounces + 1, seed);
-                else
-                {
-                    auto R0 = (eta - 1) * (eta - 1) / (eta + 1) / (eta + 1);
-                    auto c = 1 - fabs(Vector3f::dot(eta > 1 ? direction : incoming, normal));
-                    auto R = R0 + (1 - R0) * c * c * c * c * c;
-                    color = (1 - R) * traceRay({p, direction}, EPSILON, bounces + 1, seed) +
-                            R * traceRay({p, reflect_direction}, EPSILON, bounces + 1, seed) * color;
-                }
-                break;
-            }
-            }
-            return color;
-        }
-        else
+        if (!group->intersect(ray, hit, t_min) || bounces >= 100)
             return scene->getBackgroundColor(ray.getDirection());
+
+        auto p = ray.pointAtParameter(hit.getT());
+        auto material = hit.getMaterial();
+        auto normal = hit.getNormal().normalized();
+        auto incoming = ray.getNormalizedDirection();
+        Vector3f color = material->getColor(hit, p);
+        Vector3f dir_reflect = mirrorDirection(normal, incoming);
+        Vector3f dir_refract;
+        double eta = material->getRefractionIndex();
+        if (material->getType() == ILLUMINANT)
+            return color;
+        double possibility = std::max(std::max(p[0], p[1]), p[2]);
+        if (bounces >= max_bounces)
+        {
+            if (erand48(seed) < possibility)
+                color = color / possibility;
+            else
+                return scene->getBackgroundColor();
+        }
+
+        switch (material->getType())
+        {
+        case DIFFUSE:
+            return color * traceRay({p, hit.getRandomReflect(incoming, seed)}, EPSILON, bounces + 1, seed);
+        case SPECULAR:
+            return color * traceRay({p, dir_reflect}, EPSILON, bounces + 1, seed);
+        case REFRACTIVE:
+            assert(eta > 1);
+            if (Vector3f::dot(normal, incoming) < 0)
+                eta = 1 / eta;
+            if (!transmittedDirection(normal, incoming, eta, 1, dir_refract))
+                // total relection
+                return color * traceRay({p, dir_reflect}, EPSILON, bounces + 1, seed);
+            else
+            {
+                auto R0 = (eta - 1) * (eta - 1) / (eta + 1) / (eta + 1);
+                auto c = 1 - fabs(Vector3f::dot(eta > 1 ? dir_refract : incoming, normal));
+                auto R = R0 + (1 - R0) * c * c * c * c * c;
+                possibility = 0.25 + R / 2;
+                if (bounces)
+                    // Mento Carlo by Russian roulette
+                    if (erand48(seed) < possibility)
+                        return color * R / possibility * traceRay({p, dir_reflect}, EPSILON, bounces + 1, seed);
+                    else
+                        return color * (1 - R) / (1 - possibility) * traceRay({p, dir_refract}, EPSILON, bounces + 1, seed);
+                else
+                    // trace two direction for the first time
+                    return color * ((1 - R) * traceRay({p, dir_refract}, EPSILON, bounces + 1, seed) +
+                                    R * traceRay({p, dir_reflect}, EPSILON, bounces + 1, seed));
+            }
+        default:
+            assert(0);
+        }
     }
 
 private:
